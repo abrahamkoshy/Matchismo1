4bf4db0216d313c5039e03f073195849b62ce92c
Abe Koshy <abe@ubikwity.com>
2013-12-27 11:52:33 -0600

    initial commit

--- Matchismo1/CardGameViewController.m
+++ Matchismo1/CardGameViewController.m
@@ -1,29 +1,49 @@
 //
 //  CardGameViewController.m
-//  Matchismo1
+//  Matchismo
 //
-//  Created by Abraham Koshy on 12/27/13.
+//  Created by Abraham Koshy on 12/24/13.
 //  Copyright (c) 2013 Ubikwity. All rights reserved.
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (nonatomic) Deck* deck;
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+-(void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        UIImage *cardImage = [UIImage imageNamed:@"cardback"];
+        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
+    } else {
+        Card* card = [self.deck drawRandomCard];
+        if (card) {
+            UIImage *cardImage = [UIImage imageNamed:@"cardfront"];
+            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+            [sender setTitle:card.contents forState:UIControlStateNormal];
+            self.flipCount++;
+        }
+    }
 }
+
+-(Deck*)deck {
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
 
 @end
